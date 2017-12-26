6f6203d24c626b346b0595c7fa158910ab3528ef
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-26 08:32:39 -0800

    assignement. pretty complicated. worth taking another look at. used add event listner and showed how directive could call a function.

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,55 @@
 
             </div>
         </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <button v-customOn:click="clicked">Click here!</button>
+            </div>
+
+        </div>
+        <hr />
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <div
+              style="width:100px; height:100px; background-color: lightgreen"
+              v-customOn:mouseenter="mouseEntered"
+              v-customOn:mouseleave="mouseLeft"
+              >
+          </div>
+        </div>
+
+        <hr />
+
+
     </div>
 </template>
 
 <script>
     export default {
+      methods: {
+        clicked(){
+          alert('Clicked')
+        },
+        mouseEntered(){
+          console.log('mouse entered');
+        },
+        mouseLeft(){
+          console.log('mouse left');
+        }
+      },
       directives: {
+        customOn: {
+          bind(el, binding){
+            // el.onclick = function() {
+            //   binding.value();
+            // }
+            const type = binding.arg;
+            const fn = binding.value;
+            el.addEventListener(type, fn);
+          }
+        },
         'local-highlight': {
-          bind(el, binding, vnode){
+          bind(el, binding){
             var delay = 0;
             if(binding.modifiers['delayed']){
               delay = 3000;
