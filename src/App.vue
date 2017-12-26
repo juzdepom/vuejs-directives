449639ac805e14a3f1b555414453f705fa0cab0c
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-26 07:58:36 -0800

    using multiple modifiers

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
-                <p v-local-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="'red'">Color this</p>
 
             </div>
         </div>
@@ -29,13 +29,30 @@
             if(binding.modifiers['delayed']){
               delay = 3000;
             }
-            setTimeout(() => {
-              if(binding.arg == 'background'){
-                el.style.backgroundColor = binding.value;
-              } else {
-                el.style.color = binding.value;
-              }
-            }, delay);
+            if(binding.modifiers['blink']){
+              let mainColor = binding.value;
+              let secondColor = 'blue';
+              let currentColor = mainColor;
+              setTimeout(() => {
+                setInterval(() => {
+                  if(currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor);
+                  if(binding.arg == 'background'){
+                    el.style.backgroundColor = currentColor;
+                  } else {
+                    el.style.color = currentColor;
+                  }
+                }, 1000);
+              }, delay);
+            } else {
+              setTimeout(() => {
+                if(binding.arg == 'background'){
+                  el.style.backgroundColor = binding.value;
+                } else {
+                  el.style.color = binding.value;
+                }
+              }, delay);
+            }
+
           }
         }
       }
